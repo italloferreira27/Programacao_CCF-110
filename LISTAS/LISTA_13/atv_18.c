11af863fc744f7ff89d26e68ba8770fa3c73f59f
Ítallo Ferreira <italloferreirainfo@gmail.com>
2022-07-17 19:42:33 -0300

    New commit 17/07/2022

--- LISTAS/LISTA_13/atv_18.c
+++ LISTAS/LISTA_13/atv_18.c
@@ -15,17 +15,17 @@ void crescente(int * vet){
     int troca;
     for(int i = 0; i < 10; i++){
         for(int j = 0; j < 10; j++){
-        if(vet[i] < vet[j]){
-            troca = vet[i];
-            vet[i] = vet[j];
-            vet[j] = troca;
-        }
+            if(vet[i] < vet[j]){
+                troca = vet[i];
+                vet[i] = vet[j];
+                vet[j] = troca;
+            }
         }
     }
     printf("Vetor ordenado: ");
     for(int i = 0; i < 10; i++){
-    printf("%d", vet[i]);
-    printf(" ");
+        printf("%d", vet[i]);
+        printf(" ");
     }
     return 0;
 }
